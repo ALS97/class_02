1f5c5d17b71df9e7c1f5707a1c6a5530f33cba23
Austin Smith <austinleesmith97@gmail.com>
2019-10-01 13:27:45 -0700

    Got the first question working

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+var userSnow = prompt('Have you ever gone snowboarding?');
+userSnow = userSnow.toLowerCase();
+
+if (userSnow === 'yes') {
+    console.log('Awesome! It is my favorite hobby.');
+} else {
+    console.log('You should try! It is my favorite hobby.');
+
+}
