5040ba049fe12d554af486ec950ffa42ec0bc28e
Austin Smith <austinleesmith97@gmail.com>
2019-10-01 14:19:14 -0700

    Added username interface

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-'use strict'
+'use strict';
 
+var userName = prompt('What is your name, user?');
+console.log('Welcome ' + userName);
 
 var userSnow = prompt('Have you ever gone snowboarding?');
 userSnow = userSnow.toLowerCase();
 
 if (userSnow === 'yes' || userSnow === 'y') {
-    console.log('Awesome! It is my favorite hobby.');
+  console.log('Awesome! It is my favorite hobby.');
 } else {
-    console.log('You should try! It is my favorite hobby.');
+  console.log('You should try! It is my favorite hobby.');
 
 }
 
-var userTat = prompt('Do you have any tattoos?')
+var userTat = prompt('Do you have any tattoos?');
 userTat = userTat.toLowerCase();
 
 if (userTat === 'yes' || userTat === 'y') {
-    console.log('Nice! I have 6 myself.');
+  console.log('Nice! I have 6 myself.');
 } else {
-    console.log('Plenty of blank space to get some!')
-} 
+  console.log('Plenty of blank space to get some!');
+}
 
-var userTrav = prompt('Have you traveled much?')
+var userTrav = prompt('Have you traveled much?');
 userTrav = userTrav.toLowerCase();
 
 if (userTrav === 'yes' || userTrav === 'y') {
-    console.log('I have traveled to 7 different countries!');
+  console.log('I have traveled to 7 different countries!');
 } else {
-    console.log('There is still time! I have traveled quite a bit in my short time.')
-} 
+  console.log('There is still time! I have traveled quite a bit in my short time.');
+}
 
-var userHT = prompt('Are you local to the area originally?')
+var userHT = prompt('Are you local to the area originally?');
 userHT = userHT.toLowerCase();
 
 if (userHT === 'yes' || userHT === 'y') {
-    console.log('Me too! I was born in Bremerton and that is where I live now!');
+  console.log('Me too! I was born in Bremerton and that is where I live now!');
 } else {
-    console.log('Well welcome to my home state!')
-} 
+  console.log('Well welcome to my home state!');
+}
 
-var userTech = prompt('Do you have much experience in the tech industry?')
+var userTech = prompt('Do you have much experience in the tech industry?');
 userTech = userTech.toLowerCase();
 
 if (userTech === 'yes' || userTech === 'y') {
-    console.log('One step ahead! The closest I got was building robots with erector sets.');
+  console.log('One step ahead! The closest I got was building robots with erector sets.');
 } else {
-    console.log('Clean slate and no bad habits! I know very little about software, I like hardware.')
-} 
-
+  console.log('Clean slate and no bad habits! I know very little about software, I like hardware.');
+}
+console.log('It was nice meeting you, ' + userName );
 
 
