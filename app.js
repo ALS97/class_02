31ea22e3a6d42bc80e26070ac01cefe42159330e
Austin Smith <austinleesmith97@gmail.com>
2019-10-01 13:45:29 -0700

    added second question cdwr

--- app.js
+++ app.js
@@ -4,9 +4,18 @@
 var userSnow = prompt('Have you ever gone snowboarding?');
 userSnow = userSnow.toLowerCase();
 
-if (userSnow === 'yes') {
+if (userSnow === 'yes' || userSnow === 'y') {
     console.log('Awesome! It is my favorite hobby.');
 } else {
     console.log('You should try! It is my favorite hobby.');
 
 }
+
+var userTat = prompt('Do you have any tattoos?')
+userTat = userTat.toLowerCase();
+
+if (userTat === 'yes' || userTat === 'y') {
+    console.log('Nice! I have 6 myself.');
+} else {
+    console.log('Plenty of blank space to get some!')
+} 
